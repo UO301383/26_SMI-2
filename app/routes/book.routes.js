def3e7206f706bd6d6c08f9dd65f8a60c503babf
fjsuarez <fjsuarez@uniovi.es>
2023-02-18 20:31:08 +0100

    nuevos comentarios

--- app/routes/book.routes.js
+++ app/routes/book.routes.js
@@ -2,8 +2,11 @@ module.exports = app => {
     const controller = require("../controllers/book.controller");
     const validator = require('../middlewares/validators/book.validator');
     const baseRoute = '/books'
-
-    app.get(`${baseRoute}`, controller.getAll);								// Retrieve all books
-    app.post(`${baseRoute}`, validator.validateBook, controller.create);	// Create a new book
-    app.get(`${baseRoute}/:id`, controller.get);							// Get an existing book by id
+    
+    // Retrieve all books
+    app.get(`${baseRoute}`, controller.getAll);								
+    // Create a new book
+    app.post(`${baseRoute}`, validator.validateBook, controller.create);	
+    // Get an existing book by id
+    app.get(`${baseRoute}/:id`, controller.get);							
 };
