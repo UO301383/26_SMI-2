5fba4a1252f0f87b4cb0f216850e2e2d35f0ce53
cesarBallesteros <uo275912@uniovi.es>
2026-04-01 16:28:07 +0200

    Acabado archivo api.js

--- volumes/public/js/api.js
+++ volumes/public/js/api.js
@@ -12,7 +12,7 @@ function getHearders(withAuth = false) {
     }
 }
 
-// Obtener todos los videos (con búsqueda opcional))
+// obtener todos los videos (con búsqueda opcional))
 async function getVideos(search) {
     let url;
     if (search) {
@@ -24,37 +24,37 @@ async function getVideos(search) {
     return response.json();
 }
 
-//Obtener un video por su ID
+//obtener un video por su ID
 async function getVideoById(id) {
     const response = await fetch(URL + '/video/' + id);
     return response.json();
 }
 
-//Obtener videos de un usuario concreto
+//obtener videos de un usuario concreto
 async function getVideosByUser(userId) {
     const response = await fetch(URL + '/video/user/' + userId);
     return response.json();
 }
 
-//Obtener playlist
+//obtener playlist
 async function getPlaylist(){
     const response = await fetch(URL + '/playlist');
     return response.json();
 }
 
-//Obtener todos los usuarios
+//obtener todos los usuarios
 async function getUsers() {
     const response = await fetch(URL + '/users');
     return response.json();
 }
 
-//Obtener un usuario por su ID
+//obtener un usuario por su ID
 async function getUserById(id) {
     const response = await fetch(URL + '/user/' + id);
     return response.json();
 }
 
-//Actualizar nombre y username del usuario en sesión (requiere autenticación)
+//actualizar nombre y username del usuario en sesión (requiere autenticación)
 async function updateUser(id, userData) {
     const res = await fetch(URL + /user/, {
         method: 'PUT',
@@ -62,4 +62,50 @@ async function updateUser(id, userData) {
         body: JSON.stringify(userData)
     });
     return res.json();
+}
+
+//inicar sesión
+async function login(email, password) {
+    const res = await fetch(URL + '/login', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ email, password })
+    });
+    return res.json();
+}
+
+//registrar usuario
+async function register(username, email, password) {
+    const res = await fetch(URL + '/register', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ username, email, password })
+    });
+    return res.json();
+}
+
+//guardar la sesion en localStorage después de iniciar sesión o registrarse
+function saveSession(token, user){
+    localStorage.setItem('token', user);
+    localStorage.setItem('user', JSON.stringify(user));
+}
+
+//cerrar sesión
+function logout(){
+    localStorage.removeItem('token');
+    localStorage.removeItem('user');
+}
+
+//devuelve el usuario en sesión (si hay token, devuelve el usuario almacenado en localStorage, de lo contrario devuelve null)
+function getSessionUser(){
+    const user = localStorage.getItem('user');
+    if (user){
+        return JSON.parse(user);
+    }
+    return null;
+}
+
+//devuelve true si hay sesion activa, false si no
+function isLoggedIn(){
+    return !!localStorage.getItem('token');
 }
