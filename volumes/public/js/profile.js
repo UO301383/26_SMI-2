dd9559020297f123b82b0fce9205212cf7201586
cesarBallesteros <uo275912@uniovi.es>
2026-04-04 12:20:02 +0200

    empezado profile.js

---
+++ volumes/public/js/profile.js
@@ -0,0 +1,38 @@
+//espera a que el html esté completamente cargado
+document.addEventListener('DOMContentLoaded', async function() {
+
+    requiereAuth();
+    botonesAuth();
+
+    const usuario = getSessionUser();
+
+    // muestra los datos del perfil
+    document.getElementById('user-name').textContent = usuario.username;
+    document.getElementById('user-username').textContent = '@' + usuario.username;
+
+    //Pone el avatar del usuario si tiene uno, sino pone el avatar por defecto
+    if (usuario.avatar) {
+        document.getElementById('user-avatar').src = usuario.avatar;
+    }
+    // carga los videos del usuario
+    await cargarVideosUsuario(usuario.id);
+});
+
+async function cargarVideosUsuario(userId) {
+    const grid = document.getElementById('video-grid');
+    grid.innerHTML = '<p class="text-center">Cargando videos...</p>';
+
+    const videos = await fetchVideosByUserId(userId); //llama a api.js para obtener los videos del usuario
+
+    if (videos.length === 0) {
+        grid.innerHTML = '<p class="text-center">No has subido ningún video aún.</p>';
+        return;
+    }
+
+    grid.innerHTML = ''; //limpia el mensaje de carga
+
+    videos.forEach(function(video) {
+        grid.innerHTML += crearTarjetaVideo(video);
+    });
+}
+
