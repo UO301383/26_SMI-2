8fbcbb70480a2be02b02c2eb2b140328f5d5fd56
cesarBallesteros <uo275912@uniovi.es>
2026-04-10 09:05:17 +0200

    segundo intento arreglo boton registrarse

--- volumes/public/js/api.js
+++ volumes/public/js/api.js
@@ -79,7 +79,7 @@ async function registro(username, email, password) {
     const res = await fetch(baseURL + '/signup', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ name, email, password })
+        body: JSON.stringify({ name: username, username, email, password })
     });
     return res.json();
 }
